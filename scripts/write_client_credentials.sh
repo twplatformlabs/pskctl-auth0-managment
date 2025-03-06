51a9ba9b1d848a96a8af47c3befb3a4bb2e3d2a1
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-03-06 16:36:00 -0600

    [nc] add credential write logic to support parameterized client definition
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/write_client_credentials.sh
+++ scripts/write_client_credentials.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+export client=$1
+
 if [[ -f "client_credentials.json" ]]; then
-  echo "write auth0 application client credentials"
+  echo "write auth0 $client client credentials"
   Auth0ClientCredentials=$(jq . < client_credentials.json)
   TenantClientID=$(echo $Auth0ClientCredentials | jq .client_id | sed 's/"//g' | tr -d \\n)
   TenantClientSecret=$(echo $Auth0ClientCredentials | jq .client_secret | sed 's/"//g' | tr -d \\n)
 
-  op item edit 'svc-auth0' $TENANT-cli-client-id=$TenantClientID --vault platform >/dev/null
-  op item edit 'svc-auth0' $TENANT-cli-client-secret=$TenantClientSecret --vault platform >/dev/null
+  op item edit 'svc-auth0' $client-cli-client-id=$TenantClientID --vault platform >/dev/null
+  op item edit 'svc-auth0' $client-cli-client-secret=$TenantClientSecret --vault platform >/dev/null
 else
   echo "Existing client updates. No new credentials generated."
 fi
