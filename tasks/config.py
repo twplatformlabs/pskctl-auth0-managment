3edd36ff5fd1d10e496a48ec5cacb1238ad4f191
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-03-06 16:37:40 -0600

    [nc] add simple script logic to support additional explicit client
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- tasks/config.py
+++ tasks/config.py
@@ -1,8 +1,9 @@
 """global values for configure_auth0 scripts"""
 import os
 
-TENANT = os.environ.get('TENANT')
 DOMAIN = os.environ.get('DOMAIN')
+TENANT = os.environ.get('TENANT')
+BASIC_AUTH = os.environ.get('BASIC_AUTH')
 MANAGEMENT_API_CLIENT_ID = os.environ.get('MANAGEMENT_API_CLIENT_ID')
 MANAGEMENT_API_CLIENT_SECRET = os.environ.get('MANAGEMENT_API_CLIENT_SECRET')
 MANAGEMENT_API_URL = f"https://{DOMAIN}/api/v2/"
--- tasks/idp.py
+++ tasks/idp.py
@@ -2,7 +2,7 @@
 # pylint: disable=import-error,unused-argument
 from invoke import task
 from auth0.management import Auth0
-from config import DOMAIN, TENANT
+from config import DOMAIN, TENANT, BASIC_AUTH
 from authentication import get_admin_access_token
 from client import create_auth0_client
 from connection import create_auth0_connection
@@ -12,9 +12,16 @@ from action import create_auth0_action
 def install(ctx):
     """install all psk auth0 customizations"""
     auth0 = Auth0(DOMAIN, get_admin_access_token())
+    #client_id = create_auth0_client(auth0, BASIC_AUTH)
     client_id = create_auth0_client(auth0, TENANT)
     create_auth0_connection(auth0, "github", client_id)
     create_auth0_action(auth0, "set-claims-as-github-teams")
+
+@task
+def install_basic(ctx):
+    """install all psk auth0 customizations"""
+    auth0 = Auth0(DOMAIN, get_admin_access_token())
+    client_id = create_auth0_client(auth0, BASIC_AUTH)
 
 @task
 def install_client(ctx):
