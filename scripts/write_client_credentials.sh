50175771e169687396e786c4952b5991047ae3ee
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-03-09 16:07:00 -0500

    [nc] change web-authentication name, simplify workflow
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/write_client_credentials.sh
+++ scripts/write_client_credentials.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-export client=$1
+export tenant=$1
+export web=$2
 
-if [[ -f "$client-client_credentials.json" ]]; then
-  echo "write auth0 $client client credentials"
-  Auth0ClientCredentials=$(jq . < $client-client_credentials.json)
+if [[ -f "$tenant-client_credentials.json" ]]; then
+  echo "write auth0 $tenant cli client credentials"
+  Auth0ClientCredentials=$(jq . < $tenant-client_credentials.json)
   TenantClientID=$(echo $Auth0ClientCredentials | jq .client_id | sed 's/"//g' | tr -d \\n)
   TenantClientSecret=$(echo $Auth0ClientCredentials | jq .client_secret | sed 's/"//g' | tr -d \\n)
 
-  op item edit 'svc-auth0' $client-cli-client-id=$TenantClientID --vault platform >/dev/null
-  op item edit 'svc-auth0' $client-cli-client-secret=$TenantClientSecret --vault platform >/dev/null
+  op item edit 'svc-auth0' $tenant-cli-client-id=$TenantClientID --vault platform >/dev/null
+  op item edit 'svc-auth0' $tenant-cli-client-secret=$TenantClientSecret --vault platform >/dev/null
+else
+  echo "Existing client updates. No new credentials generated."
+fi
+
+if [[ -f "$web-client_credentials.json" ]]; then
+  echo "write auth0 $web client credentials"
+  Auth0ClientCredentials=$(jq . < $web-client_credentials.json)
+  TenantClientID=$(echo $Auth0ClientCredentials | jq .client_id | sed 's/"//g' | tr -d \\n)
+  TenantClientSecret=$(echo $Auth0ClientCredentials | jq .client_secret | sed 's/"//g' | tr -d \\n)
+
+  op item edit 'svc-auth0' $web-client-id=$TenantClientID --vault platform >/dev/null
+  op item edit 'svc-auth0' $web-client-secret=$TenantClientSecret --vault platform >/dev/null
 else
   echo "Existing client updates. No new credentials generated."
 fi
